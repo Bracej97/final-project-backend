f82c401028db88a5738ea7a1354ee436579a1bb1
Joe Bracey <joebracey1@gmail.com>
2024-12-18 19:30:45 +0000

    Update event time ot have a defualt

--- geekSchoolApp/models.py
+++ geekSchoolApp/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Event(models.Model):
     event_name = models.CharField(max_length=100)
     event_description = models.CharField(max_length=500)
-    event_time = models.CharField(max_length=500)
+    event_time = models.CharField(max_length=500, default="25/12/2024 18:00-20:00")
     event_attendees = models.ManyToManyField(User)
     event_location = models.CharField(max_length=300)
 
