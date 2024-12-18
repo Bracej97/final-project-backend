ccbd12f0c386990d4363e311bf589ba2f6cb4b61
Joe Bracey <joebracey1@gmail.com>
2024-12-18 19:21:36 +0000

    Updated event model

--- geekSchoolApp/models.py
+++ geekSchoolApp/models.py
@@ -5,14 +5,10 @@ from django.contrib.auth.models import User
 class Event(models.Model):
     event_name = models.CharField(max_length=100)
     event_description = models.CharField(max_length=500)
-    event_in_future = models.BooleanField(default=True)
-    start_datetime = models.DateTimeField()
-    end_datetime = models.DateTimeField()
+    event_time = models.CharField(max_length=500)
     event_attendees = models.ManyToManyField(User)
     event_location = models.CharField(max_length=300)
-    created_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_events')
-    updated_time = models.DateTimeField(auto_now=True)
-    updated_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='updated_events')
+
 
     def __str__(self):
         return f'{self.event_name}'
