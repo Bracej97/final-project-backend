fc3539dad0e249f9eba0a4bd000a83431efe0ca8
Joe Bracey <joebracey1@gmail.com>
2024-12-19 13:00:41 +0000

    Rest framework updated

--- geekSchool/settings.py
+++ geekSchool/settings.py
@@ -106,8 +106,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',  # If using JWT
+        'rest_framework.authentication.BasicAuthentication'
     ),
 }
 
