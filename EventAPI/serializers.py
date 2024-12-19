ed784899abfaf530534ce4d562be87a5b5ee56bb
Joe Bracey <joebracey1@gmail.com>
2024-12-19 19:54:05 +0000

    Testing events serializer will __all__

--- EventAPI/serializers.py
+++ EventAPI/serializers.py
@@ -4,12 +4,4 @@ from geekSchoolApp.models import Event
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = [
-            'event_name',
-            'event_description',
-            'event_date',
-            'event_start',
-            'event_end',
-            'event_attendees',
-            'event_location'
-            ]
+        fields = '__all__'
